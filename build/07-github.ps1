e4ad76fab9b56a4574333162684371b65906da7f
Wouter Tinus <win.acme.simple@gmail.com>
2024-12-24 13:12:35 +0100

    github release

--- build/07-github.ps1
+++ build/07-github.ps1
@@ -1,6 +1,6 @@
 ﻿# Something, something, Github...
 $tag = "v$version"
-gh release create $tag --generate-notes --draft --prerelease # --verify-tag
+gh release create $tag --generate-notes --draft --prerelease --verify-tag
 Get-ChildItem $Final | Where-Object { $_.Extension -eq ".zip" } | Foreach-Object {
-   gh release upload $tag $_.FullName
+   gh release upload $tag $_.FullName --clobber
 }
