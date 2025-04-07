eecedbf21113b963bf61e55ca3e345b1aded0d36
Wouter Tinus <win.acme.simple@gmail.com>
2025-04-07 22:20:39 +0200

    add null plugin

---
+++ src/main.lib/Plugins/ValidationPlugins/Dns/Null/Null.cs
@@ -0,0 +1,24 @@
+﻿using PKISharp.WACS.Context;
+using PKISharp.WACS.Plugins.Base.Capabilities;
+using PKISharp.WACS.Plugins.Base.Factories;
+using PKISharp.WACS.Plugins.Interfaces;
+using PKISharp.WACS.Services.Serialization;
+using System;
+using System.Threading.Tasks;
+
+namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
+{
+    [IPlugin.Plugin<
+        NullOptions, PluginOptionsFactory<NullOptions>,
+        DnsValidationCapability, WacsJsonPlugins>
+        ("a37b41dc-b45a-42fe-8d81-82ca409a5491",
+        "none", "Domains should be pre-authorized with the server outside of simple-acme",
+        Name = "None")]
+    class Null : IValidationPlugin
+    {
+        public ParallelOperations Parallelism => ParallelOperations.Answer | ParallelOperations.Prepare | ParallelOperations.Reuse;
+        public Task CleanUp() => Task.CompletedTask;
+        public Task Commit() => Task.CompletedTask;
+        public Task PrepareChallenge(ValidationContext context) => throw new InvalidOperationException();
+    }
+}
---
+++ src/main.lib/Plugins/ValidationPlugins/Dns/Null/NullOptions.cs
@@ -0,0 +1,6 @@
+﻿using PKISharp.WACS.Plugins.Base.Options;
+
+namespace PKISharp.WACS.Plugins.ValidationPlugins.Dns
+{
+    internal class NullOptions : ValidationPluginOptions { }
+}
