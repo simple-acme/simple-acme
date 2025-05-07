245aaa657177087e7ffc4f908747df5c0100cce1
Wouter Tinus <win.acme.simple@gmail.com>
2025-05-07 16:00:15 +0200

    default full publish

--- build/publish-local.ps1
+++ build/publish-local.ps1
@@ -18,8 +18,8 @@ $env:APPVEYOR_REPO_TAG_NAME = Get-YamlValue "releasetag" $yaml
 $env:APPVEYOR_BUILD_VERSION = Get-YamlValue "releasebuild" $yaml
 $env:APPVEYOR_REPO_COMMIT = Get-YamlValue "commit" $yaml
 
-#.\06-prepare-release.ps1
-#.\07-github.ps1
-#.\08-nuget.ps1
-#.\09-docs.ps1
+.\06-prepare-release.ps1
+.\07-github.ps1
+.\08-nuget.ps1
+.\09-docs.ps1
 .\10-chocolatey.ps1
