a2ef41249f5a34e875e831d03302b90be65492e6
Wouter Tinus <win.acme.simple@gmail.com>
2025-06-17 21:53:51 +0200

    forgot a word

--- src/main.lib/Configuration/Settings/Types/ExecutionSettings.cs
+++ src/main.lib/Configuration/Settings/Types/ExecutionSettings.cs
@@ -35,7 +35,7 @@ namespace PKISharp.WACS.Configuration.Settings.Types
 
         [SettingsValue(
             SubType = "path",
-            Description = "Path to a script that is called after attempting to renew any certificate. This may be useful to undo any actions taken by the script configured as the <code>DefaultPreExecutionScript</code>. Not to be confused with the <a href=\"/reference/plugins/installation/script\">script installation</a> plugin. The difference between these two that the installation plugin can be configured separately for each renewal and has access to a lot more context about the new and previous certificates. Also when the installation script fails, the renewal will be retried later. That is not the case for the pre/post execution scripts. Any errors there are logged but otherwise ignored.",
+            Description = "Path to a script that is called after attempting to renew any certificate. This may be useful to undo any actions taken by the script configured as the <code>DefaultPreExecutionScript</code>. Not to be confused with the <a href=\"/reference/plugins/installation/script\">script installation</a> plugin. The difference between these two is that the installation plugin can be configured separately for each renewal and has access to a lot more context about the new and previous certificates. Also when the installation script fails, the renewal will be retried later. That is not the case for the pre/post execution scripts. Any errors there are logged but otherwise ignored.",
             Tip = "Click <a href=\"https://github.com/simple-acme/reference-scripts/tree/main/Execution\">here</a> for examples."
         )]
         public string? DefaultPostExecutionScript { get; set; }
