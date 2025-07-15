8ea60ffbda668867d133dd2e62e524c5835e13e8
Wouter Tinus <wouter@simple-acme.com>
2025-07-15 20:36:53 +0200

    Webnames ca/main (#178)
    * Initial creation of Webnames DNS validation plugin (before testing)
    
    * Fixing API URL override argument
    Improving documentation
    
    * update submodule
    
    ---------
    
    Co-authored-by: jordanr <jordan@webnames.ca>

---
+++ src/plugin.validation.dns.webnames/WebnamesArguments.cs
@@ -0,0 +1,22 @@
+﻿using PKISharp.WACS.Configuration;
+using PKISharp.WACS.Configuration.Arguments;
+
+namespace PKISharp.WACS.Plugins.ValidationPlugins;
+
+/// <summary>
+/// Command line arguments for unattended mode.
+/// In this case --APIUsername and --APIKey.
+/// Where --APIKey is marked as Secret so 
+/// it will not be reflected in the logs.
+/// </summary>
+public sealed class WebnamesArguments : BaseArguments
+{
+    [CommandLine(Description = "Webnames API Username")]
+    public string? APIUsername { get; set; }
+
+    [CommandLine(Description = "Webnames API Key", Secret = true)]
+    public string? APIKey { get; set; }
+
+    [CommandLine(Description = "Webnames API Base URL Override (leave blank except for testing/debugging)", Default = null)]
+    public string? APIOverrideBaseURL { get; set; }
+}
