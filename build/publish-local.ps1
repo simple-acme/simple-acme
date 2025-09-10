8c3986432d191785fbed359bc155df204a521f5b
Wouter Tinus <win.acme.simple@gmail.com>
2025-09-10 22:17:45 +0200

    post-release cleanup

--- build/publish-local.ps1
+++ build/publish-local.ps1
@@ -20,10 +20,10 @@ $env:APPVEYOR_BUILD_VERSION = Get-YamlValue "releasebuild" $yaml
 $env:APPVEYOR_REPO_COMMIT = Get-YamlValue "commit" $yaml
 
 .\06-prepare-release.ps1
-#.\07-github.ps1
-#.\08-nuget.ps1
-#.\09-docs.ps1
-#.\10-chocolatey.ps1
+.\07-github.ps1
+.\08-nuget.ps1
+.\09-docs.ps1
+.\10-chocolatey.ps1
 .\11-winget.ps1
 
 Pop-Location
