cf34adfc0430f3180beb52b4bda076aa7e937064
帅得被人砍 <2567851772@qq.com>
2025-11-26 16:20:33 +0800

    Added the ability to use Qiniu SSL as the repository

---
+++ src/wacs.store.qiniu/QiNiuOptions.cs
@@ -0,0 +1,25 @@
+﻿using PKISharp.WACS.Plugins.Base.Options;
+using PKISharp.WACS.Services;
+using PKISharp.WACS.Services.Serialization;
+using System.Text.Json.Serialization;
+
+namespace PKISharp.WACS.Plugins.StorePlugins
+{
+    [JsonSerializable(typeof(QiNiuOptions))]
+    internal partial class QiNiuJson : JsonSerializerContext
+    {
+        public QiNiuJson(WacsJsonPluginsOptionsFactory optionsFactory) : base(optionsFactory.Options)
+        {
+        }
+    }
+    internal class QiNiuOptions : StorePluginOptions
+    {
+        public ProtectedString? Password { get; set; }
+
+        public string? QiNiuServer { get; set; }
+
+        public ProtectedString? AccessKey { get; set; }
+
+        public ProtectedString? SecretKey { get; set; }
+    }
+}
