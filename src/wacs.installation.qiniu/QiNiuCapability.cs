7df56729141cc23a0931ce9b950ab29de20e9aef
帅得被人砍 <2567851772@qq.com>
2025-11-26 16:24:10 +0800

    Added the capability to enable HTTPS certificates for Qiniu's CDN based on Qiniu's SSL certificate repository

---
+++ src/wacs.installation.qiniu/QiNiuCapability.cs
@@ -0,0 +1,28 @@
+﻿using PKISharp.WACS.Plugins.Base.Capabilities;
+using PKISharp.WACS.Plugins.Interfaces;
+using System;
+using System.Collections.Generic;
+using PKISharp.WACS.Plugins.StorePlugins;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKISharp.WACS.Plugins.InstallationPlugins
+{
+    internal class QiNiuCapability: InstallationCapability
+    {
+
+        public override State CanInstall(IEnumerable<Type> storeTypes, IEnumerable<Type> installationTypes)
+        {
+            foreach (var type in storeTypes)
+            {
+                if (type.Name == "QiNiu") { 
+                    return State.EnabledState();
+                }
+            }
+            return State.DisabledState("Requires QiNiu store plugin");
+        }
+
+    }
+}
---
+++ src/wacs.installation.qiniu/QiNiuOptionsFactory.cs
@@ -0,0 +1,56 @@
+﻿using PKISharp.WACS.Clients.IIS;
+using PKISharp.WACS.Configuration;
+using PKISharp.WACS.Plugins.Base.Factories;
+using PKISharp.WACS.Services;
+using PKISharp.WACS.Services.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKISharp.WACS.Plugins.InstallationPlugins
+{
+    internal class QiNiuOptionsFactory(ArgumentsInputService arguments) : PluginOptionsFactory<QiNiuOptions>
+    {
+
+        private ArgumentResult<ProtectedString?> AccessKey => arguments.
+            GetProtectedString<QiNiuArguments>(x => x.AccessKey).
+            Required();
+
+        private ArgumentResult<ProtectedString?> SecretKey => arguments.
+            GetProtectedString<QiNiuArguments>(x => x.SecretKey).
+            Required();
+
+        private ArgumentResult<string?> QiNiuServer => arguments.
+            GetString<QiNiuArguments>(x => x.QiNiuServer).
+            Required();
+
+        public override async Task<QiNiuOptions?> Aquire(IInputService inputService, RunLevel runLevel)
+        {
+            return new QiNiuOptions
+            {
+                QiNiuServer = await QiNiuServer.Interactive(inputService).WithLabel("Qiniu Cloud Server").GetValue(),
+                AccessKey = await AccessKey.Interactive(inputService).WithLabel("Qiniu Cloud AccessKey").GetValue(),
+                SecretKey = await SecretKey.Interactive(inputService).WithLabel("Qiniu Cloud SecretKey").GetValue(),
+            };
+        }
+
+        public override async Task<QiNiuOptions?> Default()
+        {
+            return new QiNiuOptions
+            {
+                QiNiuServer = await QiNiuServer.GetValue(),
+                AccessKey = await AccessKey.GetValue(),
+                SecretKey = await SecretKey.GetValue(),
+            };
+        }
+
+        public override IEnumerable<(CommandLineAttribute, object?)> Describe(QiNiuOptions options)
+        {
+            yield return (QiNiuServer.Meta, options.QiNiuServer);
+            yield return (AccessKey.Meta, options.AccessKey);
+            yield return (SecretKey.Meta, options.SecretKey);
+        }
+    }
+}
