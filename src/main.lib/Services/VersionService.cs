6c7a52451b296840acb7a347744376df77a8bda7
Wouter Tinus <wouter@simple-acme.com>
2026-04-09 23:15:25 +0200

    test for reparse point (symlink) when deciding on exe path

--- src/main.lib/Services/VersionService.cs
+++ src/main.lib/Services/VersionService.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-#if !DEBUG
 using System.Diagnostics;
-#endif
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -12,44 +10,76 @@ namespace PKISharp.WACS.Services
     {
         internal static bool Init(ILogService log)
         {
-            if (ExePath == null)
+            var exeFileInfo = ExeFileInfo;
+            if (exeFileInfo == null || exeFileInfo.Directory == null)
             {
                 log.Error("Unable to determine main module filename.");
                 return false;
             }
-            var processInfo = new FileInfo(ExePath);
 
             // Check for running as local .NET tool
-            if (processInfo.Name == "dotnet.exe")
+            if (exeFileInfo.Name == "dotnet.exe")
             {
                 log.Error("Running as a local dotnet tool is not supported. Please install using the --global option.");
                 return false;
             }
+
+            // Defaults
+            ExePath = exeFileInfo.FullName;
+            var path = exeFileInfo.Directory.FullName + Path.DirectorySeparatorChar;
+            SettingsPath = path;
+            PluginPath = path;
+            ResourcePath = path;
+
             // Check for running as global .NET tool
-            if (processInfo.Name == "wacs.dll")
+            if (exeFileInfo.Name == "wacs.dll" && !Debug)
             {
-#if !DEBUG
                 DotNetTool = true;
-                PluginPath = processInfo.DirectoryName!;
-                processInfo = new FileInfo(Process.GetCurrentProcess().MainModule?.FileName!);
+                var processInfo = new FileInfo(Process.GetCurrentProcess().MainModule?.FileName!);
                 ExePath = processInfo.FullName;
                 SettingsPath = Path.Combine(processInfo.Directory!.FullName, ".store", "simple-acme");
-#endif
+                PluginPath = exeFileInfo.DirectoryName!;
+                ResourcePath = AppContext.BaseDirectory;
             }
+
             log.Verbose("ExePath: {ex}", ExePath);
-            log.Verbose("ResourcePath: {ex}", ResourcePath);
-            log.Verbose("PluginPath: {ex}", PluginPath);
+            if (DotNetTool)
+            {
+                log.Verbose("ResourcePath: {ex}", ResourcePath);
+                log.Verbose("PluginPath: {ex}", PluginPath);
+                log.Verbose("SettingsPath: {ex}", SettingsPath);
+            }
             Valid = true;
             return true;
         }
 
+        internal static FileInfo? ExeFileInfo
+        {
+            get
+            {
+                var cmd = Environment.GetCommandLineArgs().First();
+                var fi = new FileInfo(cmd);
+                if (!fi.Exists)
+                {
+                    return null;
+                }
+                if (fi.Attributes.HasFlag(FileAttributes.ReparsePoint))
+                {
+                    if (fi.ResolveLinkTarget(true) is FileInfo target)
+                    {
+                        return target;
+                    }
+                }
+                return fi;
+            }
+        }
+
         internal static bool Valid { get; private set; } = false;
         internal static bool DotNetTool { get; private set; } = false;
-        internal static string SettingsPath { get; private set; } = AppContext.BaseDirectory;
-        internal static string BasePath { get; private set; } = AppContext.BaseDirectory;
-        internal static string PluginPath { get; private set; } = AppContext.BaseDirectory;
-        internal static string ExePath { get; private set; } = Environment.GetCommandLineArgs().First();
-        internal static string ResourcePath { get; private set; } = AppContext.BaseDirectory;
+        internal static string SettingsPath { get; private set; } = string.Empty;
+        internal static string PluginPath { get; private set; } = string.Empty;
+        internal static string ExePath { get; private set; } = string.Empty;
+        internal static string ResourcePath { get; private set; } = string.Empty;
         internal static string Bitness => Environment.Is64BitProcess ? "64-bit" : "32-bit";
         internal static bool Pluggable =>
 #if PLUGGABLE
