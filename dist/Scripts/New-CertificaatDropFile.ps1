fbcb0800fe5b2a306f48648a2dd11693c19fca9d
Bankeybiharidassa <bankey.biharidassa@gmail.com>
2026-04-22 11:31:31 +0100

    Rename bridge event source wording to Certificate

--- dist/Scripts/New-CertificaatDropFile.ps1
+++ dist/Scripts/New-CertificaatDropFile.ps1
@@ -27,7 +27,7 @@ function Write-BridgeEvent {
         [int]$EventId = 3001
     )
 
-    $source = 'Certificaat'
+    $source = 'Certificate'
     if (-not [System.Diagnostics.EventLog]::SourceExists($source)) {
         New-EventLog -LogName Application -Source $source
     }
@@ -36,7 +36,7 @@ function Write-BridgeEvent {
 }
 
 if ([string]::IsNullOrWhiteSpace($env:CERTIFICAAT_DROP_DIR)) {
-    Write-BridgeEvent -Message 'CERTIFICAAT_DROP_DIR is not set. Unable to write certificaat drop file.' -EntryType Error -EventId 3002
+    Write-BridgeEvent -Message 'CERTIFICAAT_DROP_DIR is not set. Unable to write certificate drop file.' -EntryType Error -EventId 3002
     exit 1
 }
 
@@ -47,7 +47,7 @@ if (-not (Test-Path -LiteralPath $dropDir)) {
 }
 
 try {
-    $probeName = ".certificaat_write_test_{0}.tmp" -f ([guid]::NewGuid().ToString('N'))
+    $probeName = ".certificate_write_test_{0}.tmp" -f ([guid]::NewGuid().ToString('N'))
     $probePath = Join-Path -Path $dropDir -ChildPath $probeName
     [System.IO.File]::WriteAllText($probePath, 'probe', [System.Text.Encoding]::UTF8)
     Remove-Item -LiteralPath $probePath -Force -ErrorAction Stop
